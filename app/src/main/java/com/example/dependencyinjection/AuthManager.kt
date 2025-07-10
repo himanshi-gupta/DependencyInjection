1e685fb42da4b7809ebb196a83ec924b315a854d
himanshi-gupta <himanshi.mca20.du@gmail.com>
2025-07-11 00:11:51 +0530

    Implemented Manual DI

--- app/src/main/java/com/example/dependencyinjection/AuthManager.kt
+++ app/src/main/java/com/example/dependencyinjection/AuthManager.kt
@@ -1,9 +1,9 @@
 package com.example.dependencyinjection
 
-class AuthManager{
-    private val signInService = SignInService()
-    private val emailService = EmailService()
-
+class AuthManager(
+    private val signInService: SignInService,
+    private val emailService: EmailService
+){
     fun login(email: String, password: String){
         signInService.signIn(email, password)
         emailService.send(to = email, from = "noreply@app.com",body = "Welcome to App")
