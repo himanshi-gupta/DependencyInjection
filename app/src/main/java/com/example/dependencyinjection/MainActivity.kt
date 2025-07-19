6e8466eb0189d72c16308fe810ee91e812ded587
himanshi-gupta <himanshi.mca20.du@gmail.com>
2025-07-19 10:10:41 +0530

    Updated Fn

--- app/src/main/java/com/example/dependencyinjection/MainActivity.kt
+++ app/src/main/java/com/example/dependencyinjection/MainActivity.kt
@@ -10,7 +10,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        val authManager = DaggerAuthManagerComponent.builder().build().getAuthManager()
+        val authManager = DaggerAuthManagerComponent.create().getAuthManager()
         authManager.login("user@example.com", "password123")
         setContent {
             DependencyInjectionTheme {
