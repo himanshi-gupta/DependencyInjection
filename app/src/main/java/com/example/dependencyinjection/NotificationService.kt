1792b0b31f29b2f43279ca7cb5b54f4668cc6d91
himanshi-gupta <himanshi.mca20.du@gmail.com>
2025-07-28 13:17:06 +0530

    Refacted code for loose coupling using interfaces

---
+++ app/src/main/java/com/example/dependencyinjection/NotificationService.kt
@@ -0,0 +1,23 @@
+package com.example.dependencyinjection
+
+import android.util.Log
+import javax.inject.Inject
+
+interface NotificationService{
+
+    fun send(to : String, from : String, body : String)
+}
+
+class EmailService @Inject constructor(): NotificationService{
+
+    override fun send(to : String, from : String, body : String){
+        Log.d("DI","Email Sent to $to!")
+    }
+}
+
+class MessageService @Inject constructor(): NotificationService{
+
+    override fun send(to : String, from : String, body : String){
+        Log.d("DI","Email Sent to $to!")
+    }
+}
