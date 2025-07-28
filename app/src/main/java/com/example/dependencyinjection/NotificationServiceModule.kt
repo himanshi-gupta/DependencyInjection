cc393dac9b11e83364199fe89488c51aa51e79a3
himanshi-gupta <himanshi.mca20.du@gmail.com>
2025-07-28 13:58:39 +0530

    @Binds for Binding

--- app/src/main/java/com/example/dependencyinjection/NotificationServiceModule.kt
+++ app/src/main/java/com/example/dependencyinjection/NotificationServiceModule.kt
@@ -1,18 +1,17 @@
 package com.example.dependencyinjection
 
+import dagger.Binds
 import dagger.Module
 import dagger.Provides
 
 @Module
-class NotificationServiceModule {
+abstract class NotificationServiceModule {
 
 //    @Provides
 //    fun getMessageService() : NotificationService{
 //        return MessageService()
 //    }
 
-    @Provides
-    fun getEmailService(emailService: EmailService) : NotificationService{
-        return emailService
-    }
+    @Binds
+    abstract fun getEmailService(emailService: EmailService) : NotificationService
 }
